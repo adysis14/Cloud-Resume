05e46aade7717b67ea45aadd7bc713c9d9097277
Aadya Singh <68416322+adysis14@users.noreply.github.com>
2022-04-14 20:30:25 +0530

    Add files via upload

--- frontend/main.js
+++ frontend/main.js
@@ -1,19 +1,22 @@
-window.addEventListener('DOMContentLoaded', (event)=>{
+window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
-})
+});
 
-const functionApi = '';
-    
+
+const localFunctionURL = 'http://localhost:7071/api/Counter';
+const productionFunctionURL = '';
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(localFunctionURL)
+    .then(response => {
         return response.json()
-    }).then(response =>{
+    })
+    .then(response => {
         console.log("Website called function API.");
         count = response.count;
-        document.getElementById("counter").innerText = count;
-    }).catch(function(error){
+        document.getElementById('counter').innerText = count;
+    }).catch(function(error) {
         console.log(error);
-    });
+      });
     return count;
 }
